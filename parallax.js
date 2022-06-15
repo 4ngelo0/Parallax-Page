001d24f00731c575390dd0cf83821d87746b780c
4ngelo0 <gabrielb4rr00s@gmail.com>
2022-06-15 07:44:49 -0300

    script corrigido

--- parallax.js
+++ parallax.js
@@ -6,26 +6,21 @@ $(function() {
         $('.parallax').each(function() {
             var obj = $(this)
 
-            // Função para garantir que iremos somente trabalhar com o elemento visível na tela 
+            // Função para garantir que iremos somente trabalhar com o elemento visível na tela
             if (windows_scrolltop >= obj.position().top - obj.height() && windows_scrolltop <= obj.position().top + obj.height()) {
-
                 // Geração do atributo para controlar a velocidade da animação
-                var divisor = typeof obj.attr('data-divisor') == 'undefined' ? 4 : obj.attr('data-dvisor')
+                var divisor = typeof obj.attr('data-divisor') == 'undefined' ? 4 : obj.attr('data-divisor')
 
-                // coreção em tela do primeiro elemento
+                // Correção em tela do primeiro elemento
                 if (obj.is(':first-child')) {
                     var bg_pos = (windows_scrolltop - obj.position().top) / divisor
                 } else {
                     var bg_pos = (windows_scrolltop - obj.position().top + (obj.height() - 100))
                 }
 
-                //  Modifica a posição do background
-                obj.css({
-                    'background-position': '50%' + bg_pos + '%'
-                })
 
                 // Animação do primeiro texto
-                obj.children('.texto').css({
+                obj.children('.textos').css({
                     'bottom': (windows_scrolltop - obj.position().top + 100) + vh
                 })
             }
